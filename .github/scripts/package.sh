41ca488d185d0153052ad04878e4c63f52717b56
almahmoud <amahmou4@jhu.edu>
2020-03-30 17:49:08 -0400

    Final github workflow test

---
+++ .github/scripts/package.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+[ -z "${GIT_TOKEN}" ] && {
+    echo 'Missing input "GIT_TOKEN: ${{ secrets.GITHUB_TOKEN }}".';
+    exit 1;
+};
+[ -z "${CHARTS_REPO}" ] && {
+    echo 'Missing input "CHARTS_REPO: cloudve/helm-charts".';
+    exit 1;
+};
+
+IFS='' read -ra CHARTS_DIR <<< "${CHARTS_REPO}"
+
+set -e
+
+BRANCH=${CHARTS_BRANCH:-master}
+CHARTS_DIR=$(basename $CHARTS_REPO)
+
+echo "Pushing to branch $CHARTS_BRANCH of repo $CHARTS_REPO";
+
+cd galaxy && rm -rf charts && rm -f requirements.lock && helm dependency update && cd ..
+git pull "${CHARTS_REPO}"
+helm package ./galaxy/ -d "${CHARTS_DIR}/charts"
+cd "${CHARTS_DIR}" && helm repo index . --url "https://raw.githubusercontent.com/${CHARTS_REPO}/${BRANCH}/"
+
+REMOTE="https://${GITHUB_ACTOR}:${GIT_TOKEN}@github.com/${CHARTS_REPO}.git"
+
+git push "${REMOTE}" HEAD:${BRANCH};
