a8d783df673437af5896cc90b4ce4f7b5a351ca9
almahmoud <amahmou4@jhu.edu>
2020-03-30 17:49:08 -0400

    Final github workflow test

---
+++ .github/scripts/extract_version.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import yaml
+import os
+
+with open(".github/tmp/version", 'w') as v:
+    with open("galaxy/Chart.yaml", 'r') as chart:
+        d = yaml.safe_load(chart)
+        v.write(d['version'])
+with open(".github/tmp/bump", 'w') as b:
+    labels = [l.get("name") for l in os.environ['GITHUB_CONTEXT']['labels']]
+    if "patch_bump" in labels:
+        b.write("patch")
+    elif "minor_bump" in labels:
+        b.write("minor")
+    elif "major_bump" in labels:
+        b.write("major")
