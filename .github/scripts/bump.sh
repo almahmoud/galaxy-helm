5fbbe754748da53e6f06cec9a49aa92b627ec00d
almahmoud <amahmou4@jhu.edu>
2020-03-30 17:49:08 -0400

    Final github workflow test

---
+++ .github/scripts/bump.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+git remote -v
+git pull
+if test (git diff --name-status origin/"${GIT_BRANCH:-master}" | grep -c "galaxy/Chart.yml") = 0 ; then
+    mkdir .github/tmp
+    python .github/scripts/extract_version.py
+    if [ -f .github/tmp/bump ]; then
+        bump2version --current-version $(cat .github/tmp/version) $(cat .github/tmp/bump) ./galaxy/Chart.yaml
+        rm -rf .github/tmp
+        git config --local user.email "action@github.com"
+        git config --local user.name "GitHub Action"
+        git add .
+        git commit -m "Automatic Version Bumping"
+
+        set -e
+        REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
+
+        echo "Push to branch $GIT_BRANCH";
+        [ -z "${GIT_TOKEN}" ] && {
+            echo 'Missing input "GIT_TOKEN: ${{ secrets.GITHUB_TOKEN }}".';
+            exit 1;
+        };
+
+        REMOTE="https://${GITHUB_ACTOR}:${GIT_TOKEN}@github.com/${REPOSITORY}.git"
+
+        git push "${REMOTE}" HEAD:${GIT_BRANCH};
+    fi
+fi
